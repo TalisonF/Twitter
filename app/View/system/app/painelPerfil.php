e260d8c8bf665e0151fbbba0111fe6930df412be
TalisonF <talison737@gmail.com>
2019-03-29 21:20:17 -0300

    Painel

--- app/View/system/app/painelPerfil.php
+++ app/View/system/app/painelPerfil.php
@@ -1,37 +1,35 @@
-<div class="col-md-3">
-
-			<div class="perfil">
-				<div class="perfilTopo">
-
-				</div>
-
-				<div class="perfilPainel">
-					
-					<div class="row mt-2 mb-2">
-						<div class="col mb-2">
-							<span class="perfilPainelNome"><?=$_SESSION['nome']?></span>
-						</div>
-					</div>
-
-					<div class="row mb-2">
-
-						<div class="col">
-							<span class="perfilPainelItem">Tweets</span><br />
-							<span class="perfilPainelItemValor"><?=$QtdeTweets?></span>
-						</div>
-
-						<div class="col">
-							<span class="perfilPainelItem"> <a class="quemSeguirTxt" href="/app/seguindo">Seguindo</a></span><br />
-							<span class="perfilPainelItemValor"><?=$qtdeSeguindo?></span>
-						</div>
-
-						<div class="col">
-							<span class="perfilPainelItem"><a class="quemSeguirTxt" href="/app/seguidores">Seguidores</a></span><br/>
-							<span class="perfilPainelItemValor"><?=$qtdeSeguidores?></span>
-						</div>
-					</div>
-
+<div class="col-md-3 col-sm-12">
+		
+	<div class="perfil">
+			<!--Avatar-->
+			<div class="fundo-avatar p-3">				
+				<center> <img src="/img/icon.png" alt="avatar" class="avatar"></center>
+			</div>
+			
+			<!--fim avatar -->
+			
+			<!--nome -->
+			<div class="row mt-2 mb-2 p-2">
+				<div class="col mb-2">
+					<span class="perfilPainelNome"><?=$_SESSION['nome']?></span>
 				</div>
 			</div>
-
-		</div>
+			<!--fim nome -->
+			<!--Parte inferior-->
+				<div class="row mb-2 p-2">
+					<div class="col">
+						<span class="perfilPainelItem">Tweets</span><br />
+						<span class="perfilPainelItemValor"><?=$QtdeTweets?></span>
+					</div>
+					<div class="col">
+						<span class="perfilPainelItem"> <a class="quemSeguirTxt" href="/app/seguindo">Seguindo</a></span><br />
+						<span class="perfilPainelItemValor"><?=$qtdeSeguindo?></span>
+					</div>
+					<div class="col">
+						<span class="perfilPainelItem"><a class="quemSeguirTxt" href="/app/seguidores">Seguidores</a></span><br/>
+						<span class="perfilPainelItemValor"><?=$qtdeSeguidores?></span>
+					</div>
+				</div>
+			<!--Fim Parte inferior-->
+	</div>
+</div>
